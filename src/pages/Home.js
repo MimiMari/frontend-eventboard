523161a9d727b8b88566cc95aca5aa6c29012ff2
MimiMari <marieangele@gmx.de>
2022-09-20 20:16:06 +0200

    useEffect, load Events, useStat and css formatting

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
 export default function Home() {
+
+    const [events,setEvents]=useState([])
+
+    useEffect(() => {
+        loadEvents();
+        console.log("test logging")
+    }, []);
+
+    const loadEvents=async()=>{
+        const result=await axios.get("http://localhost:8080/events");
+        console.log(result.data)
+        setEvents(result.data);
+    }
+
   return (
     <div className="container">
-      <div className='py-4'></div>
-        <tr>
-        <div className="border border-dark rounded m-2 p-3">
-        <th>
-        <p>Title: some Title</p>
-        <p>Date: some Date</p>
-        <p>Place: some Place</p>
-        </th>
-        <th className="my-2">Description: some Description</th>
-       </div>
-        </tr>
+      <div className='py-4'>
+                {
+                  events.map((event,index)=>(
+                    <tr>
+                      <div className="border border-dark rounded m-2 p-3">
+                        <th>
+                        <p key={index}>{index+1}</p>
+                        <p>Title: {event.title}</p>
+                        <p>Date: {event.date}</p>
+                        <p>Place: {event.place}</p>
+                        </th>
+                        <th className="my-2">Description: {event.description}</th>
+                      </div>
+                    </tr>
+                    ))
+                }
+      </div>
     </div>
   )
 }
